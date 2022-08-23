553ee4729c73d539a5c6c6868f8f9f51ee91002c
rahenry1 <rajadurai.henry@gmail.com>
2022-08-23 16:33:46 -0500

    Base ui for CoSigner confirmation page

--- hackproject/code/smartdid/src/components/Complete/Complete.tsx
+++ hackproject/code/smartdid/src/components/Complete/Complete.tsx
@@ -1,6 +1,11 @@
 import Button from '@mui/material/Button/Button';
 import React, { FC } from 'react';
 import styles from './Complete.module.css';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { styled } from '@mui/material/styles';
+import SendIcon from '@mui/icons-material/Send';
+import Grid from '@mui/material/Grid';
 
 interface CompleteProps {
   handleNext: any
@@ -15,8 +20,296 @@ const Complete: FC<CompleteProps> = (props) =>
 
   return (
   <div className={styles.Complete}>
-    Complete Component
-    <Button variant="contained"  onClick={nextStep}>Complete</Button>
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 4, width: '25ch' },
+             p: 1,
+             m: 5,
+//              justifyContent: 'center',
+//              border: '1px solid grey',
+             boxShadow: 1,
+          }}
+          noValidate
+          autoComplete="off" >
+          Congratulations. <b>Loan Application 12345</b> is approved. Now you can download the Digital Student Loan Certificates into your Digital Wallet app.
+
+          <Button variant="contained"  endIcon={<SendIcon />} onClick={nextStep}>Complete</Button>
+
+          </Box>
+
+         <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 4, width: '25ch' },
+             p: 1,
+             m: 5,
+             justifyContent: 'space-evenly',
+//              border: '1px solid grey',
+             boxShadow: 1,
+          }}
+          noValidate
+          autoComplete="off" >
+          <h2>CoSigner Details</h2>
+           <TextField
+               id="fname"
+               label="First name"
+               defaultValue="Mike"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+              <TextField
+                id="mname"
+                label="Middle name"
+                defaultValue=""
+                InputProps={{
+                  readOnly: true,
+                }}
+                variant="standard"
+              />
+             <TextField
+               id="lname"
+               label="Last name"
+               defaultValue="Doe"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+             <TextField
+               id="dob"
+               label="Date of Birth"
+               defaultValue="01/01/1999"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+             <TextField
+               id="ssn"
+               label="SSN"
+               defaultValue="000-000-0000"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+             <TextField
+               id="citizenship"
+               label="Citizenship"
+               defaultValue="U.S. Citizen"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+           <h4> Contact Details</h4>
+             <TextField
+               id="address"
+               label="Address"
+               defaultValue="1234, new streat, city, state, 60000"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+             <TextField
+               id="country"
+               label="Country of Residence"
+               defaultValue="USA"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+             <TextField
+               id="email"
+               label="Email"
+               defaultValue="email"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+             <TextField
+               id="phone"
+               label="Phone"
+               defaultValue="000-000-0000"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+           <h4> Income Details</h4>
+             <TextField
+               id="employmentStatus"
+               label="Employment Status"
+               defaultValue="full time employed"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+             <TextField
+               id="grossAnnualIncome"
+               label="Gross Annual Income"
+               defaultValue="Amount"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+             <TextField
+               id="monthlyHousingPayment"
+               label="Monthly Housing Payment"
+               defaultValue="Amount"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+        </Box>
+
+         <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 4, width: '25ch' },
+             p: 1,
+             m: 5,
+             justifyContent: 'space-evenly',
+//              border: '1px solid grey',
+             boxShadow: 1,
+          }}
+          noValidate
+          autoComplete="off" >
+        <h2>College Details</h2>
+        <TextField
+               id="collegeState"
+               label="College State"
+               defaultValue="State"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+             <TextField
+               id="collegeName"
+               label="College Name"
+               defaultValue="College Name"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+             <TextField
+               id="loanType"
+               label="Loan Type"
+               defaultValue="Undergraduate Loan"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+        </Box>
+
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 4, width: '25ch' },
+             p: 1,
+             m: 5,
+             justifyContent: 'space-evenly',
+//              border: '1px solid grey',
+             boxShadow: 1,
+          }}
+          noValidate
+          autoComplete="off" >
+          <h2>Student Details</h2>
+           <TextField
+               id="fname"
+               label="First name"
+               defaultValue="John"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+              <TextField
+                id="mname"
+                label="Middle name"
+                defaultValue=""
+                InputProps={{
+                  readOnly: true,
+                }}
+                variant="standard"
+              />
+             <TextField
+               id="lname"
+               label="Last name"
+               defaultValue="Doe"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+             <TextField
+               id="dob"
+               label="Date of Birth"
+               defaultValue="01/01/1999"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+             <TextField
+               id="ssn"
+               label="SSN"
+               defaultValue="000-000-0000"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+             <TextField
+               id="citizenship"
+               label="Citizenship"
+               defaultValue="U.S. Citizen"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+           <h4> Contact Details</h4 >
+             <TextField
+               id="address"
+               label="Address"
+               defaultValue="1234, new streat, city, state, 60000"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+             <TextField
+               id="email"
+               label="Email"
+               defaultValue="email"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+             <TextField
+               id="phone"
+               label="Phone"
+               defaultValue="000-000-0000"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+        </Box>
   </div>
 )};
 
--- hackproject/code/smartdid/src/components/ConfirmDetails/ConfirmDetails.tsx
+++ hackproject/code/smartdid/src/components/ConfirmDetails/ConfirmDetails.tsx
@@ -3,14 +3,8 @@ import styles from './ConfirmDetails.module.css';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
-import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import SendIcon from '@mui/icons-material/Send';
 
 interface ConfirmDetailsProps {
   handleNext: any
@@ -40,7 +34,7 @@ const ConfirmDetails: FC<ConfirmDetailsProps> = (props) => {
           Thank you for your <b>Loan Application 12345</b>.  We need a CoSigner for your application to complete the loan.
 
           <b>CoSignerKey 4321</b> please share this Key with your CoSigner to resume your application.
-
+          <Button variant="contained"  color="success" endIcon={<SendIcon />} onClick={nextStep}>Complete</Button>
           </Box>
 
 
@@ -86,7 +80,7 @@ const ConfirmDetails: FC<ConfirmDetailsProps> = (props) => {
              />
         </Box>
 
-         <Box
+        <Box
           component="form"
           sx={{
             '& .MuiTextField-root': { m: 4, width: '25ch' },
@@ -136,18 +130,28 @@ const ConfirmDetails: FC<ConfirmDetailsProps> = (props) => {
                variant="standard"
              />
              <TextField
-               id="address"
-               label="Address"
-               defaultValue="Hello World"
+               id="ssn"
+               label="SSN"
+               defaultValue="000-000-0000"
                InputProps={{
                  readOnly: true,
                }}
                variant="standard"
              />
              <TextField
-               id="country"
-               label="Country"
-               defaultValue="Country"
+               id="citizenship"
+               label="Citizenship"
+               defaultValue="U.S. Citizen"
+               InputProps={{
+                 readOnly: true,
+               }}
+               variant="standard"
+             />
+           <h4> Contact Details</h4 >
+             <TextField
+               id="address"
+               label="Address"
+               defaultValue="1234, new streat, city, state, 60000"
                InputProps={{
                  readOnly: true,
                }}
@@ -171,18 +175,7 @@ const ConfirmDetails: FC<ConfirmDetailsProps> = (props) => {
                }}
                variant="standard"
              />
-             <TextField
-               id="citizenship"
-               label="Citizenship"
-               defaultValue="U.S. Citizen"
-               InputProps={{
-                 readOnly: true,
-               }}
-               variant="standard"
-             />
         </Box>
-
-        <Button variant="contained"  onClick={nextStep}>CLOSE</Button>
      </div>
 )};
 
