2eac56a5d9a34c22acf2af7696bf7f49ecf3127f
Tarun Luthra <tluthra6@gatech.edu>
2022-08-23 18:54:15 -0400

    Added changes to display QR image

--- hackproject/code/smartdid/src/components/QrVerification/QrVerification.tsx
+++ hackproject/code/smartdid/src/components/QrVerification/QrVerification.tsx
@@ -1,6 +1,9 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect, SetStateAction } from 'react';
 import styles from './QrVerification.module.css';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box/Box';
+import axios from 'axios';
+
 
 interface QrVerificationProps {
   handleNext: any
@@ -9,13 +12,46 @@ interface QrVerificationProps {
 const QrVerification: FC<QrVerificationProps> = (props) => 
 {
 
+  const baseApiUrl = 'http://localhost:3001/';
   const nextStep = () => {
     props.handleNext(1);
   }
 
+  const [img, setImg] = useState<string>();
+
+  const fetchImage = async () => {
+    const res = await fetch(baseApiUrl+'getVerifyInviteURL');
+    const imageBlob = await res.blob();
+    const imageObjectURL = URL.createObjectURL(imageBlob);
+    setImg(imageObjectURL);
+    axios.get(baseApiUrl+'getVerifyRelationshipDid').then(data => {
+      console.log(data);
+    })
+  };
+
+  useEffect(() => {
+    fetchImage();
+  }, []);
+
   return(
     <div className={styles.QrVerification}>
       QrVerification Component
+      <article className={styles.QrImage}>
+
+      {/* <img src={`/images/${imageName}`} /> */}
+      <Box
+        component="img"
+        sx={{
+          height: 300,
+          width: 350,
+          maxHeight: { xs: 400 },
+          maxWidth: { xs: 400 },
+        }}
+
+        alt="QR Code"
+        src={`${img}`}
+      />
+      </article>
       <Button variant="contained"  onClick={nextStep}>Verify</Button>
     </div>
   );
